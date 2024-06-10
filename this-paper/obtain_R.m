92a3c27893acf6fe7867033c450c62ad28710066
Oliver <37354467+Dzalwaysc@users.noreply.github.com>
2024-06-11 04:38:41 +0800

    Delete this-paper directory

--- this-paper/obtain_R.m
+++
@@ -1,7 +0,0 @@
-function R = obtain_R(State)
-phi = State(3);
-R_ = [cos(phi) -sin(phi)  0;
-      sin(phi)  cos(phi)  0;
-         0        0       1];
-R = reshape(R_, 9, 1);
-end
--- this-paper/obtain_dEta.m
+++
@@ -1,4 +0,0 @@
-function dEta = obtain_dEta(State, V)
-R = reshape(obtain_R(State), 3, 3);
-dEta = R*V';
-end
